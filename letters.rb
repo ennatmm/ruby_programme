b7704a61c8a16bdd3563681b3d18fb4fd3113bcf
ennatmm <40546882+ennatmm@users.noreply.github.com>
2018-06-25 18:57:11 +0200

    restart
    Lektion neu

--- letters.rb
+++ letters.rb
@@ -1,6 +1,3 @@
-lineWidth = 60
-str= '-->Text<--'
-puts str.ljust lineWidth
-puts str.center lineWidth
-puts str.rjust lineWidth
-puts str.ljust(lineWidth/3) + str.rjust(lineWidth/3) + str.rjust(lineWidth/3)
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
