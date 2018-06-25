7ee375937e5bc579180fd89e8865e2a30b4875b3
ennatmm <40546882+ennatmm@users.noreply.github.com>
2018-06-25 22:25:19 +0200

    interactive

---
+++ interactive.rb
@@ -0,0 +1,7 @@
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '?  What a lovely name!'
+puts 'Pleased to meet you, ' + name + '.  :)'
+puts 'Do you eventually know? Well what was his name then?'
+name2 = gets.chomp
+puts 'Yes, ' + name + ' that was his name! ' + name2
