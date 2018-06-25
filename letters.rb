1769d609cdfe2e93a6e43e378ac9f4553247934f
ennatmm <40546882+ennatmm@users.noreply.github.com>
2018-06-25 19:10:55 +0200

    complete chapter
    all about letters

--- letters.rb
+++ letters.rb
@@ -1,3 +1,12 @@
 puts 'Hello, world!'
 puts ''
 puts 'Good-bye.'
+puts 'I like ' + 'apple pie.'
+puts ('I like ' + 'apple pie. ')* 4
+puts  12  +  12
+puts '12' + '12'
+puts '12  +  12'
+puts  2  *  5
+puts '2' *  5
+puts '2  *  5'
+puts 'You\'re swell!'
